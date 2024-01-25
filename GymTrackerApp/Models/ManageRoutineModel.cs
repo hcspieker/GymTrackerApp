9e4f6164ebf9022f8bd30c8520a9f0d8e178554e
Harald Spieker <14868252+hcspieker@users.noreply.github.com>
2024-01-25 21:09:56 +0100

    added models for manage routines

---
+++ GymTrackerApp/Models/ManageRoutineModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace GymTrackerApp.Models
+{
+    public partial class ManageRoutineModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string title;
+
+        public ObservableCollection<string> WorkoutSummaries { get; set; }
+
+        public ManageRoutineModel() : this(string.Empty, new List<string>())
+        {
+
+        }
+
+        public ManageRoutineModel(string title, List<string> workoutSummaries)
+        {
+            Title = title;
+            WorkoutSummaries = new ObservableCollection<string>(workoutSummaries);
+        }
+    }
+}
--- GymTrackerApp/ViewModels/ManageRoutinesViewModel.cs
+++ GymTrackerApp/ViewModels/ManageRoutinesViewModel.cs
@@ -1,10 +1,38 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using GymTrackerApp.Models;
 using GymTrackerApp.Views;
+using System.Collections.ObjectModel;
 
 namespace GymTrackerApp.ViewModels
 {
     public partial class ManageRoutinesViewModel : BaseViewModel
     {
+        public ObservableCollection<ManageRoutineModel> Routines { get; set; }
+
+        public ManageRoutinesViewModel()
+        {
+            Routines = new ObservableCollection<ManageRoutineModel>();
+
+            for (int i = 0; i < 15; i++)
+            {
+                var workoutSummaries = new List<string>();
+
+                var amountOfWorkouts = Random.Shared.Next(1, 5);
+                for (int j = 0; j < amountOfWorkouts; j++)
+                {
+                    var amountOfExercises = Random.Shared.Next(1, 7);
+                    var workoutSummary = $"Workout {j + 1}: ";
+                    for (int k = 0; k < amountOfExercises; k++)
+                    {
+                        workoutSummary += $"Exercise {k + 1}, ";
+                    }
+                    workoutSummaries.Add(workoutSummary.Substring(0, workoutSummary.Length - 2));
+                }
+
+                Routines.Add(new ManageRoutineModel($"Routine {i + 1}", workoutSummaries));
+            }
+        }
+
         [RelayCommand]
         async Task CreateNewRoutine()
         {
@@ -21,9 +49,9 @@ namespace GymTrackerApp.ViewModels
         }
 
         [RelayCommand]
-        async Task Details(string element)
+        async Task Details(ManageRoutineModel routine)
         {
-            await Shell.Current.DisplayAlert("Info", $"Clicked on details of '{element}'", "close");
+            await Shell.Current.DisplayAlert("Info", $"Clicked on details of '{routine.Title}'", "close");
         }
     }
 }
