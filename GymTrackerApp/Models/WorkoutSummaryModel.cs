07a516d9c52d0e6d1ffaa8f9bbfc65402e155b50
Harald Spieker <14868252+hcspieker@users.noreply.github.com>
2024-01-26 22:55:38 +0100

    redesigned startpage and added models

---
+++ GymTrackerApp/Models/WorkoutSummaryModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace GymTrackerApp.Models
+{
+    public partial class WorkoutSummaryModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string routineTitle, workoutTitle;
+
+        [ObservableProperty]
+        private DateTime start, end;
+
+        public ObservableCollection<string> ExerciseSummaries { get; set; }
+
+        public WorkoutSummaryModel() : this(string.Empty, string.Empty, DateTime.Now, DateTime.Now, new())
+        {
+
+        }
+
+        public WorkoutSummaryModel(string routineTitle, string workoutTitle, DateTime start,
+            DateTime end, List<string> exerciseSummaries)
+        {
+            RoutineTitle = routineTitle;
+            WorkoutTitle = workoutTitle;
+            Start = start;
+            End = end;
+            ExerciseSummaries = new ObservableCollection<string>(exerciseSummaries);
+        }
+    }
+}
--- GymTrackerApp/ViewModels/StartViewModel.cs
+++ GymTrackerApp/ViewModels/StartViewModel.cs
@@ -1,9 +1,33 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using GymTrackerApp.Models;
+using System.Collections.ObjectModel;
 
 namespace GymTrackerApp.ViewModels
 {
     public partial class StartViewModel : BaseViewModel
     {
+        public ObservableCollection<WorkoutSummaryModel> Workouts { get; set; }
+
+        public StartViewModel()
+        {
+            Workouts = new ObservableCollection<WorkoutSummaryModel>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Workouts.Add(new WorkoutSummaryModel
+                {
+                    RoutineTitle = "Routine A",
+                    WorkoutTitle = $"Workout {(i % 2 == 0 ? "1" : "2")}",
+                    Start = DateTime.Now.AddDays(-(i + 1)),
+                    End = DateTime.Now.AddDays(-(i + 1)).AddHours(1),
+                    ExerciseSummaries = [
+                        $"Exercise 1 (5x5 1{i}0)",
+                        $"Exercise 2 (3x8 {i}0)"
+                    ]
+                });
+            }
+        }
+
         [RelayCommand]
         async Task StartTraining()
         {
@@ -11,9 +35,10 @@ namespace GymTrackerApp.ViewModels
         }
 
         [RelayCommand]
-        async Task Details(string element)
+        async Task Details(WorkoutSummaryModel element)
         {
-            await Shell.Current.DisplayAlert("Info", $"Clicked on details of '{element}'", "close");
+            await Shell.Current.DisplayAlert("Info", $"Clicked on details of {element.RoutineTitle} - " +
+                $"{element.WorkoutTitle}: {element.Start:dd.MM.yyyy HH:mm:ss} - {element.End:HH:mm:ss}", "close");
         }
     }
 }
